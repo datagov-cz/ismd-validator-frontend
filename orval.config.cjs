2192fb0cbdc0ab078e8d1d17271f275b0f7dc67c
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-05-28 21:08:39 +0200

    add axios, react query, orval. generate api endpoints

---
+++ orval.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  api: {
+    input: 'http://localhost:8080/v3/api-docs',
+    output: {
+      target: './src/api/generated.ts',
+      client: 'react-query',
+      mode: 'single',
+      prettier: true,
+      override: {
+        mutator: {
+          path: './src/axios-instance.ts',
+          name: 'axiosInstance',
+        },
+      },
+    },
+  },
+};
---
+++ src/axios-instance.ts
@@ -0,0 +1,25 @@
+import Axios, { AxiosRequestConfig } from 'axios';
+
+export const AXIOS_INSTANCE = Axios.create({
+  baseURL: 'http://localhost:8080/v3/api-docs',
+});
+
+export const axiosInstance = <T>(
+  config: AxiosRequestConfig,
+  options?: AxiosRequestConfig,
+): Promise<T> => {
+  const source = Axios.CancelToken.source();
+  const promise = AXIOS_INSTANCE({
+    ...config,
+    ...options,
+    paramsSerializer: { indexes: null },
+    cancelToken: source.token,
+  }).then(({ data }) => data);
+
+  // @ts-ignore
+  promise.cancel = () => {
+    source.cancel('Query was cancelled');
+  };
+
+  return promise;
+};
