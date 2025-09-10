69dfb144f994160f43efcfbf43fb7b31cda41b75
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-09-10 10:09:01 +0100

    add: aria-label

--- src/components/shared/ThemeSwitch.tsx
+++ src/components/shared/ThemeSwitch.tsx
@@ -1,18 +1,19 @@
 'use client';
 
 import { GovIcon } from '@gov-design-system-ce/react';
+import { useTranslations } from 'next-intl';
 
 import { useTheme } from '@/components/contexts/ThemeProvider';
-import { useState } from 'react';
 
 export const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
-  // const [theme, setTheme] = useState('light');
+  const t = useTranslations('Header');
   const isDark = theme === 'dark';
 
   return (
     <button
       onClick={() => setTheme(isDark ? 'light' : 'dark')}
+      aria-label={t(isDark ? 'ThemeSwitchAria.ToLight' : 'ThemeSwitchAria.ToDark')}
       className="relative w-10 h-6 rounded-3xl transition-colors duration-300 outline-0 focus-visible:outline-1"
       style={{ backgroundColor: isDark ? '#2362a2' : '#b0b0b0' }}
     >
